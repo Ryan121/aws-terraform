63f59d1ebff417a4275563c00082acb6c14a77dd
Ryan121 <37290593+Ryan121@users.noreply.github.com>
2022-09-05 16:50:17 +0100

    Added aws vpc

--- first_code.tf
+++ first_code.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "my_first_bucket" {
+resource "aws_s3_bucket" "prod_my_first_bucket" {
   bucket = "tf-course-aws-bucket-050922"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
